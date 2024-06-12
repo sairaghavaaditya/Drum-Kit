c4d8597c4bda45b8ed9440a3e7fa31fb97adfe5f
Aditya <sairaghavaaditya7@gmailcom>
2024-06-12 20:40:02 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,79 @@
+//button detection
+var n=document.querySelectorAll(".drum").length;
+for(var i=0;i<n;i++){
+document.querySelectorAll("button")[i].addEventListener("click", function (){
+var buttonInnerHTML=this.innerHTML;
+
+makesound(buttonInnerHTML);
+buttonanimation(buttonInnerHTML);
+});
+}
+//detecting keybord press
+document.addEventListener("keypress", function(event){
+makesound(event.key);
+buttonanimation(event.key);
+});
+
+
+
+
+//var audio=new Audio('sounds/crash.mp3');
+//audio.play();
+
+function makesound(key){
+
+
+    switch(key){
+        case "w":
+            var crash=new Audio('sounds/crash.mp3');
+            crash.play();
+            break;
+        case "a":
+            var kick=new Audio('sounds/kick-bass.mp3');
+            kick.play();
+            break;
+        case "s":
+            var snare=new Audio('sounds/snare.mp3');
+            snare.play();
+            break;
+        case "d":
+            var tom1=new Audio('sounds/tom-1.mp3');
+            tom1.play();
+            break;
+        case "j":
+            var tom2=new Audio('sounds/tom-2.mp3');
+            tom2.play();
+            break;
+        case "k":
+            var tom3=new Audio('sounds/tom-3.mp3');
+            tom3.play();
+            break;
+        case "l":
+            var tom4=new Audio('sounds/tom-4.mp3');
+            tom4.play();
+            break; 
+        default:
+             console.log();
+    
+    }
+
+}
+
+
+document.addEventListener("keypress", response(key));
+function response(event){
+console.log(event);
+}
+
+function buttonanimation(currentkey){
+
+var activebutton =document.querySelector("."+currentkey);
+activebutton.classList.add("pressed");
+
+setTimeout(function(){
+
+    activebutton.classList.remove("pressed");
+
+},100);
+
+}
